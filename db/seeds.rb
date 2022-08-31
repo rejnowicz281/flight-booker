dc047a4369d3014d251c70f86743dab295a09350
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-08-31 13:13:59 +0200

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Airport.create(location: "New York", code: "NYC")
+Airport.create(location: "San Francisco", code: "SFO")
+Airport.create(location: "Atlanta", code: "ATL")
+Airport.create(location: "Springfield", code: "SPI")
+Airport.create(location: "Boston", code: "BOS")
+Airport.create(location: "Detroit", code: "DTW")
+Airport.first.departing_flights.create(
+    start_datetime: "2023-02-02 11:00:00 UTC", 
+    duration: 2.5, 
+    arrival_airport_id: 2)
+Airport.second.departing_flights.create(
+    start_datetime: "2023-02-02 16:00:00 UTC", 
+    duration: 2.5, 
+    arrival_airport_id: 1)
+Airport.first.departing_flights.create(
+    start_datetime: "2023-02-02 20:00:00 UTC", 
+    duration: 2.7, 
+    arrival_airport_id: 3)
+Airport.fourth.departing_flights.create(
+    start_datetime: "2023-02-03 21:00:00 UTC", 
+    duration: 3, 
+    arrival_airport_id: 5)
