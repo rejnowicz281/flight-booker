1b699f548ca595c9d41a8e79ef2f9a6104932d66
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-09-03 20:28:32 +0200

    Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,46 @@ Airport.first.departing_flights.create(
     start_datetime: "2023-02-02 11:00:00 UTC", 
     duration: 2.5, 
     arrival_airport_id: 2)
+Airport.first.departing_flights.create(
+    start_datetime: "2023-02-02 12:00 UTC", 
+    duration: 2.5, 
+    arrival_airport_id: 2)
+Airport.first.departing_flights.create(
+    start_datetime: "2023-02-03 11:00:00 UTC", 
+    duration: 2.5, 
+    arrival_airport_id: 2)
 Airport.second.departing_flights.create(
     start_datetime: "2023-02-02 16:00:00 UTC", 
+    duration: 2.5, 
+    arrival_airport_id: 1)
+Airport.second.departing_flights.create(
+    start_datetime: "2023-02-02 17:00:00 UTC", 
+    duration: 2.5, 
+    arrival_airport_id: 1)
+Airport.second.departing_flights.create(
+    start_datetime: "2023-02-03 16:00:00 UTC", 
     duration: 2.5, 
     arrival_airport_id: 1)
 Airport.first.departing_flights.create(
     start_datetime: "2023-02-02 20:00:00 UTC", 
     duration: 2.7, 
     arrival_airport_id: 3)
+Airport.first.departing_flights.create(
+    start_datetime: "2023-02-02 23:00:00 UTC", 
+    duration: 2.7, 
+    arrival_airport_id: 3)
+Airport.first.departing_flights.create(
+    start_datetime: "2023-02-03 20:00:00 UTC", 
+    duration: 2.7, 
+    arrival_airport_id: 3)
+Airport.fourth.departing_flights.create(
+    start_datetime: "2023-02-02 21:00:00 UTC", 
+    duration: 3, 
+    arrival_airport_id: 5)
+Airport.fourth.departing_flights.create(
+    start_datetime: "2023-02-02 04:00:00 UTC", 
+    duration: 3, 
+    arrival_airport_id: 5)
 Airport.fourth.departing_flights.create(
     start_datetime: "2023-02-03 21:00:00 UTC", 
     duration: 3, 
