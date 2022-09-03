69c08830fd673f567e1a5a776c902d863c6ccbec
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-09-03 22:04:12 +0200

    Add name and email columns to passengers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_03_195305) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_03_200224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_03_195305) do
     t.bigint "booking_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "email"
     t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
